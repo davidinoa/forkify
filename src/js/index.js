bfa071ef37f039b25ee2f871df6e1c338a52b286
David Inoa <davidinoa@gmail.com>
2018-11-24 19:14:00 -0400

    connect to food2fork

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '5bdcd78b5134e55b310659e596b40810';
+
+  try {
+    const res = await axios(
+      `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`,
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getResults('pizza');
