27fe4e676b4cd0b95ca9066b9b1dc9554ae8b9cb
David Inoa <davidinoa@gmail.com>
2018-11-26 16:03:29 -0400

    fix title bug

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -14,9 +14,10 @@ const limitRecipeTitle = (title, limit = 17) => {
   const newTitle = [];
   if (title.length > limit) {
     title.split(' ').reduce((acc, cur) => {
-      if (acc + cur.length <= length) {
+      if (acc + cur.length <= limit) {
         newTitle.push(cur);
       }
+      return acc + cur.length;
     }, 0);
 
     return `${newTitle.join(' ')} ...`;
