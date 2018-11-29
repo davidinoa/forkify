d7af4f90318b285e1124400c6563c9359ef8bf7b
David Inoa <davidinoa@gmail.com>
2018-11-29 17:48:49 -0400

    implement recipe view

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -53,6 +53,8 @@ export default class Recipe {
       'cup',
       'pound',
     ];
+    const units = [...unitsShort, 'kg', 'g'];
+
     const newIngredients = this.ingredients.map((el) => {
       // 1) uniform units
       let ingredient = el.toLowerCase();
@@ -65,7 +67,7 @@ export default class Recipe {
 
       // 3) parse ingredients into count, unit, and ingredient
       const arrIng = ingredient.split(' ');
-      const unitIndex = arrIng.findIndex((el2) => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex((el2) => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
